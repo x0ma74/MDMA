927d8d37198220fe13838e634f3290bd216f0286
x0ma74 <rds.goldenbox@gmail.com>
2021-04-13 01:00:47 +0500

    add black + stube

---
+++ black/index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>SPHERICAL BLACK BOARDS</title>
+  <link href='https://fonts.googleapis.com/css?family=Noto+Sans:400,700' rel='stylesheet' type='text/css'>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+  <link rel="stylesheet" href="./style.css">
+
+  <style>
+    
+
+    body { 
+     background: url(https://s8.hostingkartinok.com/uploads/images/2021/04/f9af63f03a40ed2854499d3622bfa636.png); /* Фоновый рисунок */
+    }
+    div {
+     padding: 1px; /* Поля вокруг текста */
+     border-radius: 5px; /* Радиус скругления */
+     background: #3E3B32; /* Цвет фона */
+    }
+   </style>
+  
+
+</head>
+<body>
+
+<header>
+  <nav>
+    <h1>
+      <a href="http://dee.newspaper.keenetic.pro/"><span style="color:#ffffff">⠀SPHERICAL BLACK BOARDS</span></a>
+    </h1>
+    <ul>
+      
+    </ul>
+
+  </nav>
+</header>
+<br>
+<div class="container2">
+<!— Put this script tag to the <head>
+<script type="text/javascript" src="https://vk.com/js/api/openapi.js?168"></script>
+<script type="text/javascript">
+VK.init({apiId: 7208710, onlyWidgets: true});
+</script>
+<!— Put this div tag to the place, where the Comments block will be —>
+<div id="vk_comments"></div>
+<script type="text/javascript">
+VK.Widgets.Comments("vk_comments", {limit: 5, width: "auto", pageUrl: "http://dee.newspaper.keenetic.pro/black!", attach: "*"})
+</script></div></div>
+
+<center>
+<p><span style="color:#A9A9A9">⠀⠀⠀ЧТОБЫ ОПУБЛИКОВАТЬ СВОЙ КОНТЕНТ ПОСЛЕ МОДЕРАЦИИ ПРИШЛИ ЕГО В КОММЕНТАРИИИ ВЫШЕ</span><br>
+<span style="color:">⠀⠀⠀ЧТОБЫ ПОЛУЧАТЬ СООБЩЕНИЯ С УВЕДОМЛЕНИЯМИ О НОВОМ КОНТЕНТЕ |<a href="https://vk.cc/asPipJ"><span style="color:#4d7198"><b> ОФОРМИТЬ ПОДПИСКУ</span></a> |</b><br>
+</center>
+
+<main>
+
+
+
+
+
+<div class="container">
+<p><span style="color:#4d7198">⠀⠀⠀<b>
+12.04.2021</b></span><span style="color:#FFFFFF">  <b>
+SPHERICAL BLACK BOARDS</b> </p>  <span style="color:#A9A9A9"><div class="text-block"><font size="" color="">
+Лиса искренне не понимала, почему все платформы для выкладки контента направлены на однозадачных людей. Она хотела такой сервис, где сегодня можно было говорить о текстах, завтра об экологии, послезавтра о педагогике. Чтобы в ее любимом контакте и можно было поделиться с друзьями “Крысана”. Собирать алмазную мозаику, делать украшения, выращивать яблоню, обозревать книгу. В итоге не красные глаза как у “Погромиста”, а хорошее настроение и позитивный настрой. Да, так бывает, когда вообще все интересно. Когда не можешь определиться с нишами, поэтому пробуешь все. Наконец то бесконечный генератор разнопланового контента, может спокойно определиться с выбором. Однозначно spherical black boards. Настоящий выбор: <a href="https://vk.com/id285894117"><span style="color:#6B8E23"><b>Лис</b></span></a>.</font></p></div></div>
+
+
+<br><div class="container">
+<p><span style="color:#4d7198">⠀⠀⠀<b>
+12.04.2021</b></span><span style="color:#FFFFFF">  <b>
+ЗНАЧЕНИЕ ДЕНЕГ В СОВРЕМЕННОМ МИРЕ</b> </p>  <span style="color:#A9A9A9"><div class="text-block">	<font size="" color="">
+Человека вынуждают работать больше и больше за деньги, которые ему не нужны. Почему деньги не нужны? Потому что эти ненужные деньги будут покрывать избыточные потребности мнимых ценностей. Тех, что личность с заработной платой в семьдесят тысяч будет питаться и носить, по сути, то же самое, что и остальные  с зарплатой в тридцать. Неоднократно видел и знаю на опыте в лице, тех самых знакомых, что приобретают сигареты, вещи, алкоголь в несущественные по стоимости разы дешевле. Кстати, не сказать, что это сегмент низшего класса и плохого качества, но и не высший как у некоторых. Тех у кого появились всякого рода дебильные желания, вроде заказа доставки пиццы, игры в боулинг и прочие интересы. Очевидно, что такие потребности-излишества созданы для того, чтобы куда было слить лишние деньги, которыми, по сути, лишними никогда не бывают. Их просто потратить не знают куда, так сказать, найти разумное применение. Хотя мог бы работать три дня в неделю и получать удовольствие все остальное время. Да, была создана система ценностей, которая дает ориентир на то, чтобы человек стремился конвертировать свое время в труд дабы приобретать или пользоваться этими ценностями. Для чего и зачем уже ясно. Сделано это было с той целью, чтобы человек больше работал и увеличивал мировой товарооборот.</font></p></div></div>
+
+</main>
+</body>
+</html>
--- stube/backup/index.php
+++ stube/backup/index.php
@@ -25,11 +25,11 @@
 
 </head>
 <body>
-
+<!-- partial:index.partial.html -->
 <header>
   <nav>
     <h1>
-      <a href="http://dee.newspaper.keenetic.pro/"><span style="color:#ffffff">⠀STUBE</span></a>
+      <a href="http://dee.newspaper.keenetic.pro/"><span style="color:#ffffff">⠀STube</span></a>
     </h1>
     <ul>
       <a href="https://vk.cc/asPipJ"><span style="color:#000000">ПОДПИСКА</span></a>⠀|⠀<a href="https://vk.com/public183680103"><span style="color:#000000">ВКОНТАКТЕ</span></a>
@@ -38,14 +38,7 @@
 
   </nav>
 </header>
-
-<br><br><br><br>
-<p><span style="color:#FFFFFF">⠀⠀⠀ЧТОБЫ ПОЛУЧАТЬ ВКОНТАКТЕ ЛИЧНЫЕ СООБЩЕНИЯ С УВЕДОМЛЕНИЯМИ О 
-<br>⠀⠀⠀ПОЯВИВШЕМСЯ НОВОМ КОНТЕНТЕ ОФОРМИТЕ: <a href="https://vk.cc/asPipJ"><span style="color:#6B8E23">ПОДПИСКУ</span></a>
-<br><span style="color:#FFFFFF">⠀⠀⠀ЧТОБЫ ДОБАВИТЬ СВОЕ ВИДЕО В ПРИЛОЖЕНИЕ 
-<br>⠀⠀⠀ПЕРЕЙДИТЕ ПО ССЫЛКЕ: <a href="https://vk.cc/9Mjdu9"><span style="color:#6B8E23">ЗАГРУЗКА</span></a></p>
 <br>
-
 <main>
   <div class="container">
     <iframe width="640" height="480" src="https://www.youtube.com/embed/bc5jimy9Q7M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
@@ -57,6 +50,9 @@
 </div>
 
 </main>
+
+<!-- partial -->
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.12.0/lodash.min.js'></script><script  src="./script.js"></script>
 
 </body>
 </html>
